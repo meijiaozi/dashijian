5818477f2b91b7d5266c839c93afc28fe9091aab
meijiaozi <870052916@qq.com>
2020-10-27 09:44:30 +0800

    头像已完成

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+$(function(){
+    $('.logout').on('click',function(){
+        // 1.退出功能
+        layer.confirm('确定删除?', {icon: 3, title:'提示'}, function(index){
+            window.localStorage.removeItem('token')
+            location.href='./login.html'
+            layer.close(index);
+          });
+        
+    })
+
+        //   2.发送请求用户信息
+    $.ajax({
+        url:"/my/userinfo",
+        success:function(res){
+            if(res.status===0){
+            $('.username').text(res.data.username)
+            }
+            if(res.data.user_pic){
+              $(".text-avatar").hide()
+
+              $('.layui-nav-img').show().attr('src',res.data.user_pic)
+            }else{
+                $('.layui-nav-img').hide()()
+                $(".text-avatar").show().text(res.data.username.slice(0,1).toUpperCase())
+            }
+        }
+    })
+})
